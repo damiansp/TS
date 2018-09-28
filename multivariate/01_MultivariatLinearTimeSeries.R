3abf707eb81380cf8f359dc98300c23a880c75d9
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-28 08:47:55 -0700

    init 1

---
+++ multivariate/01_MultivariatLinearTimeSeries.R
@@ -0,0 +1,8 @@
+#=========#=========#=========#=========#=========#=========#=========#=========
+rm(list=ls())
+setwd('~/Learning/TS/multivariate')
+
+library(MTS)
+
+# 1. Introduction
+# 1.4 CCM (Cross-Correlation Matrix)
