e5574fca4b5674071bd6193d26ceb0499080b0ae
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-01 09:24:38 -0700

    +1: + import

--- multivariate/01_MultivariatLinearTimeSeries.R
+++ multivariate/01_MultivariatLinearTimeSeries.R
@@ -3,6 +3,11 @@ rm(list=ls())
 setwd('~/Learning/TS/multivariate')
 
 library(MTS)
+library(mvtnorm)
 
 # 1. Introduction
 # 1.4 CCM (Cross-Correlation Matrix)
+sig <- diag(2) # I[2]
+x <- rmvnorm(300, rep(0, 2), sig)
+
+??rmvnorm
