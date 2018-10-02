725152d28e9cce540bf7aff6903ccd76b25bd1df
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-02 07:48:40 -0700

    1++

--- multivariate/01_MultivariatLinearTimeSeries.R
+++ multivariate/01_MultivariatLinearTimeSeries.R
@@ -6,8 +6,13 @@ library(MTS)
 library(mvtnorm)
 
 # 1. Introduction
-# 1.4 CCM (Cross-Correlation Matrix)
+# 3 - 4. CCM (Cross-Correlation Matrix)
 sig <- diag(2) # I[2]
 x <- rmvnorm(300, rep(0, 2), sig)
+MTSplot(x)
+ccm(x)
 
-??rmvnorm
+
+
+# 5. Testing Zero Cross-Correlations
+
