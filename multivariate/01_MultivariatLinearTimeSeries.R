382048dbd3fc00ce35f0536368f9a623b07cefb9
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-08 08:39:59 -0700

    +multivariate, ignores data/

--- multivariate/01_MultivariatLinearTimeSeries.R
+++ multivariate/01_MultivariatLinearTimeSeries.R
@@ -15,4 +15,19 @@ ccm(x)
 
 
 # 5. Testing Zero Cross-Correlations
+dat <- read.table('data/q-gdpunemp.txt', header=T)
+head(dat)
+
+x <- cbind(diff(dat$gdp), diff(dat$rate))
+mq(x, lag=10) # series not independent for any lag
+
+# Sim study
+sig <- diag(3)
+z <- rmvnorm(200, rep(0, 3), sig)
+mq(z, 10) # no signif. cross correlation
+
+
+
+# 6. Forecasting
+
 
