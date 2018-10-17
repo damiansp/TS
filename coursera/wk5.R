83eebe6c06658dec81e4f15b1324f89fa3b4b79e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-17 08:38:21 -0700

    5++

--- coursera/wk5.R
+++ coursera/wk5.R
@@ -2,6 +2,8 @@
 rm(list=ls())
 setwd('~/Learning/TS/coursera')
 
+library(forecast)
+data(discoveries)
 
 # AIC
 data <- arima.sim(list(order=c(2, 0, 0), ar=c(0.7, -0.2)), n=2000)
@@ -19,3 +21,19 @@ for (p in 1:5) {
 }
 
 
+# ARMA properties
+plot(discoveries, main='Major Scientific Discoveries by Year')
+stripchart(discoveries, method='stack', offset=0.5, at=0.15, pch=19)
+
+par(mfrow=c(2, 1))
+acf(discoveries)
+pacf(discoveries)
+# examine competing models with p, q both on [0, 3]
+# ic=.. so select criterion
+auto.arima(discoveries, d=0, approximation=F) # 2 0 0; d = 0: no evidence of trend
+
+
+
+#        AR(p)                 MA(q)                 ARMA(p, q)
+# ACF    Tails off             Cuts off after lag q  Tails off
+# PACF   Cuts off after lag p  Tails off             Tails off
