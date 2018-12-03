27b84bfe31fb1ba7c555f6ce7aa69165a858fef1
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-12-03 07:40:47 -0800

    fin 5, init 6; ignore .Rapp.history

--- coursera/wk5.R
+++ coursera/wk5.R
@@ -76,8 +76,51 @@ auto.arima(sim)
 Box.test(sim, lag=log(length(sim)))
 
 
+
 # Applied: Daily Female Births in CA, 1959
 births <- read.csv('data/daily-total-female-births-in-cal.csv')
+n.births <- births$Daily.total.female.births.in.California..1959
 births$Date <- as.Date(births$Date, '%Y-%m-%d')
 head(births)
 plot(births, type='l')
+
+# Test for correlation
+Box.test(n.births, lag=log(length(n.births))) # correlated
+Box.test(rnorm(365), lag=log(365)) # cf.
+plot(diff(n.births), type='l')
+
+# Corr in differenced data?
+Box.test(diff(n.births), lag=log(length(diff(n.births)))) # correlated
+
+par(mfrow=c(3, 1))
+plot(diff(n.births), type='l')
+acf(diff(n.births), 50)
+pacf(diff(n.births), 50)
+
+# Fit various ARIMA mods
+mod1 <- arima(n.births, order=c(0, 1, 1))
+(sse1 <- sum(mod1$residuals^2)) # 18148.46
+(mod1.box <- Box.test(mod1$residuals, lag=log(length(mod1$residuals)))) # 0.5334
+
+mod2 <- arima(n.births, order=c(0, 1, 2))
+(sse2 <- sum(mod2$residuals^2)) # 17914.65
+(mod2.box <- Box.test(mod2$residuals, lag=log(length(mod2$residuals)))) # 0.9859
+
+mod3 <- arima(n.births, order=c(7, 1, 1))
+(sse3 <- sum(mod3$residuals^2)) # 17584.39
+(mod3.box <- Box.test(mod3$residuals, lag=log(length(mod3$residuals)))) # ~1
+
+mod4 <- arima(n.births, order=c(7, 1, 2))
+(sse4 <- sum(mod4$residuals^2)) # 17574.06
+(mod4.box <- Box.test(mod4$residuals, lag=log(length(mod4$residuals)))) # ~1
+
+df <- data.frame(row.names=c('AIC', 'SSE', 'p'), 
+                 c(mod1$aic, sse1, mod1.box$p.value),
+                 c(mod2$aic, sse2, mod2.box$p.value),
+                 c(mod3$aic, sse3, mod3.box$p.value),
+                 c(mod4$aic, sse4, mod4.box$p.value))
+colnames(df) <- c('ARIMA(011)', 'ARIMA(012)', 'ARIMA(711)', 'ARIMA(712)')
+format(df, scientific=F)
+
+sarima(n.births, 0, 1, 2, 0, 0, 0)
+
