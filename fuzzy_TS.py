0b83218c39a0aeb55e676bf138a3e547c86a3672
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-18 08:13:25 -0700

    rm fuzzy (outdated and unsupported lib)

--- fuzzy_TS.py
+++
@@ -1,7 +0,0 @@
-def triangular(x, a, b, c):
-    '''
-    Defines a triangle with points: left: (a, 0), top: (b, 1), right: (c, 0);
-    x is a value to be assigned affinity to the group defined by abc
-    '''
-    return max(min((x - a)/(b - a), (c - x)/(c - b)), 0)
-
