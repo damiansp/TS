632dc42ed2c0e6bfd79ee11fa3c7c785d525fc99
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-07-23 07:55:08 -0700

    init penn/2

--- penn/02_MAmodelsPACF.R
+++ penn/02_MAmodelsPACF.R
@@ -8,3 +8,8 @@ setwd('~/Learning/TS/penn')
 
 
 library(astsa)
+
+
+# 2 PACF
+ma1pacf <- ARMAacf(ma=c(0.7), lag.max=36, pacf=T)
+plot(ma1pacf, type='h', main='Theoretical PACF for MA(1) with theta=0.7')
