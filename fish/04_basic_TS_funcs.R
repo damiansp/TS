f8be94a8a18c0918b7abb096b133def1d82b1188
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-07-26 08:16:56 -0700

    +4.1

--- fish/04_basic_TS_funcs.R
+++ fish/04_basic_TS_funcs.R
@@ -17,3 +17,25 @@ library(stats)
 data(hourlyphyto)
 data(MLCO2)
 data(NHTemp)
+
+
+
+# 1. TS Plots
+
+
+# 1.1. ts Objects and plot.ts()
+head(MLCO2)
+co2 <- ts(MLCO2$ppm, frequency=12, start=c(MLCO2[1, 'year'], MLCO2[1, 'month']))
+plot.ts(co2, xlab=expression(paste('CO'[2],' (ppm)')))
+
+
+# 1.2. Combining and Plotting Multiple ts Objects
+temp.ts <- ts(NHTemp$Value, frequency=12, start=c(1880, 1))
+dat.I <- ts.intersect(co2, temp.ts)
+dim(dat.I)
+
+dat.U <- ts.union(co2, temp.ts)
+dim(dat.U)
+
+plot(dat.I, main='')
+plot(dat.U, main='')
