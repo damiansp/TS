cbb7d4c2fb3412ba27b9a08d4a539e70504a4561
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-08-01 07:56:48 -0700

    determining phi and CCM

--- multivariate/mvtsa/02_StationaryVectorAutoregressiveTS.R
+++ multivariate/mvtsa/02_StationaryVectorAutoregressiveTS.R
@@ -11,27 +11,30 @@ library(MTS)
 
 
 # 2. VAR(1) Models
+
+
 # 2.5 Moment Equations
 (phi1 <- matrix(c(0.2, -0.6, 0.3, 1.1), 2, 2))
-(sig <- matrix(c(1, 0.8, 0.8, 2), 2, 2))
-(m1 <- eigen(phi1))
+(sig <- matrix(c(1, 0.8, 0.8, 2), 2, 2)) # sig[a] (cov matrix of a)
+(m1 <- eigen(phi1))                      # |eigenvals| all < 1 so stationary
 I4 <- diag(4)
-(pp <- kronecker(phi1, phi1))
-(c1 <- c(sig))
+(pp <- kronecker(phi1, phi1))            # Kronecker product
+(c1 <- c(sig))                           # vectorize
 dd <- I4 - pp
 dd.inv <- solve(dd)
-(gam0 <- dd.inv %*% matrix(c1, 4, 1))
+(gam0 <- dd.inv %*% matrix(c1, 4, 1))    # Solve for gamma0
 
-g0 <- matrix(gam0, 2, 2)
-(g1 <- phi1 %*% g0)
+
+(g0 <- matrix(gam0, 2, 2))               # Restack
+(g1 <- phi1 %*% g0)                      # Find others recursively
 (g2 <- phi1 %*% g1)
 
-(D <- diag(sqrt(diag(g0)))) # cross correlation
+(D <- diag(sqrt(diag(g0))))              # compute cross correlation matrices
 Di <- solve(D)
 
-Di %*% g0 %*% Di
-Di %*% g1 %*% Di
-Di %*% g2 %*% Di
+(rho1 <- Di %*% g0 %*% Di)               # lag-1 CCM
+(rho2 <- Di %*% g1 %*% Di)               # lag-2
+(rho3 <- Di %*% g2 %*% Di)               # ...
 
 
 
