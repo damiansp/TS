9227dc1459c7a9bc87bd7f34f223881a6abf1335
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-08-06 07:58:10 -0700

    init 3

---
+++ penn/03_identifyingAndEstimatingARIMA.R
@@ -0,0 +1,26 @@
+#=========#=========#=========#=========#=========#=========#=========#=========
+rm(list=ls())
+lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
+       detach,
+       character.only=T,
+       unload=T)
+setwd('~/Learning/TS/penn')
+
+
+library(astsa)
+
+
+erie <- scan('https://newonlinecourses.science.psu.edu/stat510/sites/stat510/files/data/eriedata.dat')
+erie <- ts(erie)
+par(mfrow=c(3, 1))
+plot(erie, type='l')
+acf(erie)
+pacf(erie) # Good AR(1) candidate
+
+sarima(erie, 1, 0, 0) # decent diagnostics, signif AR coef; AIC = 1.469
+sarima(erie, 0, 0, 1) # resid autocorrelation; AIC = 1.726
+sarima(erie, 1, 0, 1) # diagnosicts look good, bur more params, and AIC = 1.514
+
+# Forecast sneak peek
+par(mfrow=c(1, 1))
+sarima.for(erie, 4, 1, 0, 0) # e.g. 4 steps ahead for AR(1)
