a32a43017f4ab94fa6eb7220ac9e0a3c5a21f138
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-09-24 07:49:31 -0700

    update model simplification

--- multivariate/mvtsa/02_StationaryVectorAutoregressiveTS.R
+++ multivariate/mvtsa/02_StationaryVectorAutoregressiveTS.R
@@ -111,16 +111,48 @@ mq(resi, adj=18) # adj: adjust degrees of freedom: p = 2, k = 3 (# of ts)
 
 
 # 7.3 Model Simplification
-m3 <- VARchi(z, p=2)             # default p <= 0.1
-m3 <- VARchi(z, p=2, thres=1.96) # p <= 0.05
 
+# 7.3.1 Testing Zero Parmeters for Model Reduction
+m3 <- VARchi(z, p=2)             # default thres=1.645 (p <= 0.1)
+m3 <- VARchi(z, p=2, thres=1.96) # (p <= 0.05)
+
+# Infomation-criterion based stepwise reduction
 m1 <- VAR(z, 2)
 m2 <- refVAR(m1, thres=1.96)
+# Reduced model is
+#        T 0.16 T   T 0.47 0.21    0 T          T     0 0 0 T
+# z[t] = |    0 | + | 0.33 0.27 0.50 | z[t-1] + | -0.20 0 0 | z[t-2] + a[t]
+#        L 0.28 |   L 0.47 0.23 0.23 |          L -0.30 0 0 |
+#
+# with Res Cross COV Matrix:
+# T 0.29 0.02 0.07 T
+# | 0.02 0.31 0.15 |
+# L 0.07 0.15 0.36 |
+# 
+# where
+# UK = z[1, t] = 0.16 + 0.47z[1, t-1] + 0.21z[2, t-1]                
+#                                + a[1, t]
+# CA = z[2, t] =        0.33z[1, t-1] + 0.27z[2, t-1] + 0.50z[3, t-1] 
+#                - 0.20z[1, t-2] + a[2, t]
+# US = z[3, t] = 0.28 + 0.47z[1, t-1] + 0.23z[2, t-1] + 0.23z[3, t-1]
+#                - 0.30z[1, t-2] + a[3, t]
+#
+# and Cross COR Matrix (lag0):
+# T 1.00 0.06 0.22 T
+# | 0.06 1.00 0.44 |
+# L 0.22 0.44 1.00 |
+resid(m2)
 
 
-MTSdiag(m2, adj=12)
+# Check Model Outputs
+MTSdiag(m2, adj=12) 
+# Resid CC ...basically good, pvals all non-signif; one questionable p val for 
+# lag 4 in Ljung-Box (MQ) stats
 
 
+
+
+################################################################################
 
 # 9 Forecasting
 VARpred(m1, 8) # 8 steps ahead
