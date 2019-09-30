c0c35dfa58a4521a8b4703eb9b407d95b1dcf7a4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-09-30 08:21:14 -0700

    add decomposition example

--- penn/05_smoothingAndDecomp.R
+++ penn/05_smoothingAndDecomp.R
@@ -12,3 +12,19 @@ library(astsa)
 
 
 # 1. Decomposition Models
+beer <- scan('../data/beerprod.dat')
+beer <- ts(beer, freq=4)
+beer.decomp <- decompose(beer, type='additive')
+plot(beer.decomp)
+beer.decomp
+
+beer.decomp.mult <- decompose(beer, type='multiplicative')
+plot(beer.decomp.mult)
+
+# Lowess Seasonal and Trend Decomposition
+beer.decomp.lowess <- stl(beer, 'periodic')
+plot(beer.decomp.lowess)
+
+
+
+# 2. Smoothing Time Series
