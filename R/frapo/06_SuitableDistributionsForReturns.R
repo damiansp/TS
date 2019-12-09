26137bf3e63b832cb2b9d94fe0ce8283bfc73e78
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-12-09 07:39:27 -0800

    +estimate VaR and ES

--- R/frapo/06_SuitableDistributionsForReturns.R
+++ R/frapo/06_SuitableDistributionsForReturns.R
@@ -73,7 +73,59 @@ AIC
 # p < 0.05: reject hypothesis that both mods have equal explanatory power
 
 
+# Code 6.2 VaR and ES Derived from GHP, HYP and NIG
+# probs
+p <- seq(0.001, 0.05, 0.001)
 
+# VaR
+ghd.VaR <- abs(qghyp(p, ghd.fit))
+hyp.VaR <- abs(qghyp(p, hyp.fit))
+nig.VaR <- abs(qghyp(p, nig.fit))
+nor.VaR <- abs(qnorm(p, mean=mean(yret), sd=sd(c(yret[, 1]))))
+emp.VaR <- abs(quantile(x=yret, probs=p))
 
+# VaR Plot
+plot(emp.VaR, 
+     type='l', 
+     xlab='', 
+     ylab='Value at Risk', 
+     axes=F, 
+     ylim=range(c(hyp.VaR, nig.VaR, ghd.VaR, nor.VaR, emp.VaR)))
+box()
+axis(1, at=seq(along=p), labels=names(emp.VaR), tick=F)
+axis(2, at=pretty(range(hyp.VaR, nig.VaR, ghd.VaR, nor.VaR, emp.VaR)))
+lines(seq(along=p), ghd.VaR, col=2)
+lines(seq(along=p), hyp.VaR, col=4)
+lines(seq(along=p), nig.VaR, col=3)
+lines(seq(along=p), nor.VaR, col=5)
+legend('topright', 
+       legend=c('Empirical', 'GHD', 'HYP', 'NIG', 'Normal'),
+       col=c(1, 2, 4, 3, 5),
+       lty=1)
 
+# ES
+ghd.ES <- abs(ESghyp(p, ghd.fit))
+hyp.ES <- abs(ESghyp(p, hyp.fit))
+nig.ES <- abs(ESghyp(p, nig.fit))
+nor.ES <- abs(mean(yret) - sd(c(yret[, 1])) * dnorm(qnorm(1 - p))/p)
+obs.p <- ceiling(p * length(yret))
+emp.ES <- sapply(obs.p, function(x) abs(mean(sort(c(yret))[1:x])))
 
+# ES Plot
+plot(emp.ES,
+     type='l',
+     xlab='',
+     ylab='ES',
+     axes=F,
+     ylim=range(hyp.ES, nig.ES, ghd.ES, nor.ES, emp.ES))
+box()
+axis(1, at=1:length(p), labels=names(emp.VaR))
+axis(2, at=pretty(range(hyp.ES, nig.ES, ghd.ES, nor.ES, emp.ES)))
+lines(1:length(p), ghd.ES, col=2)
+lines(1:length(p), hyp.ES, col=4)
+lines(1:length(p), nig.ES, col=3)
+lines(1:length(p), nor.ES, col=5)
+legend('topright', 
+       legend=c('Empirical', 'GHD', 'HYP', 'NIG', 'Normal'),
+       col=c(1, 2, 4, 3, 5),
+       lty=1)
