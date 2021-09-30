5ad4f9fb7bdc611d88a1956c47a664cdbf1d6d14
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-09-30 09:23:18 -0700

    r eda++

--- python/practical_TSA/03_eda.R
+++ python/practical_TSA/03_eda.R
@@ -12,3 +12,12 @@ hist(diff(EuStockMarkets[, 'SMI'], 30))
 plot(EuStockMarkets[, 'SMI'], EuStockMarkets[, 'DAX'])
 plot(diff(EuStockMarkets[, 'SMI']), diff(EuStockMarkets[, 'DAX']))
 plot(lag(diff(EuStockMarkets[, 'SMI']), 1), diff(EuStockMarkets[, 'DAX']))
+
+
+# Window functions
+x <- rnorm(100, sd=10) + 1:100
+mn <- function(n) { rep(1/n, n) }
+
+plot(x, type='l')
+lines(filter(x, mn(5)), col=2, lw=2)
+lines(filter(x, mn(25)), col=3, lw=2)
