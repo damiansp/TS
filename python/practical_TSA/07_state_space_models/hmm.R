e65610a6e0d103cbe7191a8bbe7b1b55afd9ae07
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-05-23 13:28:18 -0700

    +hmm.py

--- python/practical_TSA/07_state_space_models/hmm.R
+++ python/practical_TSA/07_state_space_models/hmm.R
@@ -1,3 +1,5 @@
+library(depmixS4)
+
 # states for a stock market
 bull.mu <- 0.1
 bull.sd <- 0.1
@@ -10,17 +12,44 @@ panic.sd <- 0.3
 
 mus <- c(bull.mu, neutral.mu, bear.mu, panic.mu)
 sds <- c(bull.sd, neutral.sd, bear.sd, panic.sd)
-N.PERIODS <- 10
+N.PERIODS <- 30
 SHORTEST.PERIOD <- 20
 LONGEST.PERIOD <- 40
 
-days <- sample(SHORTEST.PERIOD:LONGEST.PERIOD, N.PERIODS, replac=T)
+days <- sample(SHORTEST.PERIOD:LONGEST.PERIOD, N.PERIODS, replace=T)
 returns <- numeric()
 true.mean <- numeric()
 for (d in days) {
-  market.state <- sample(1:(length(mus)), 1, prob=c(0.2, 0.6, 0.18, 0.02))
+  market.state <- sample(1:(length(mus)), 1, prob=c(0.2, 0.4, 0.2, 0.1))
   returns <- c(returns, rnorm(d, mean=mus[market.state], sd=sds[market.state]))
   true.mean <- c(true.mean, rep(mus[market.state], d))
 }
 
 table(true.mean)
+cum.returns <- cumprod(c(1, 1+ returns))
+cum.returns <- cum.returns[-1]
+
+hmm.mod <- depmix(
+  returns ~ 1, family=gaussian(), nstates=4, data=data.frame(returns=returns))
+mod.fit <- fit(hmm.mod)
+post.probs <- posterior(mod.fit)
+
+par(mfrow=c(2, 1))
+YMN <- 1.2*min(returns)
+YMX <- 1.2*max(returns)
+par(mar=c(0, 4, 1, 1))
+plot(returns, type='l', ylim=c(YMN, YMX), xaxt='n')
+lapply(
+  0:length(returns) - 1, 
+  function(i) {
+  	rect(
+  	  i, 
+  	  YMN, 
+  	  (i + 1),
+  	  YMN + 0.2,
+  	  col=post.probs$state[i + 1],
+  	  border=NA)
+  })
+par(mar=c(3, 4, 1, 1))
+plot(cum.returns, type='l', log='y')  
+attr(mod.fit, 'response')
